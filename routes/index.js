8a4b42de9f7bd8d54ec2332f462f012c53da2423
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-21 01:15:24 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var requestIp = require('request-ip');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Request-Header-Parser-Microservice' });
+});
+
+router.get('/api/whoami', function (req, res) {
+	var clientIp = requestIp.getClientIp(req);
+	var	language;
+	var	software;
+	res.json({'clientIp': clientIp});
+});
+
+module.exports = router;
